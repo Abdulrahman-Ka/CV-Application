d44121b5def665f9a96bc38d50795edbd035154b
Abdulrahman-Ka <abode.kalls@gmail.com>
2025-05-23 08:38:25 +0300

    fix CvContainer.css name

--- src/components/CvContainer.jsx
+++ src/components/CvContainer.jsx
@@ -7,7 +7,6 @@ import ExperienceSection from "./ExperienceSection";
 const CvContainer = () => {
   return (
     <>
-
       <h1 className="cv-title">My CV Builder</h1>
       <div className="cv-container">
         <GeneralInfoForm />
